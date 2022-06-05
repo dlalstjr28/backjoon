7cc7cdd393a3212317c6ead4ee3f9a9ad505e4df
dlalstjr28 <48358123+dlalstjr28@users.noreply.github.com>
2022-06-05 23:11:20 +0900

    Update solve.py

--- 이분탐색/숫자 카드2(10816번)/solve.py
+++ 이분탐색/숫자 카드2(10816번)/solve.py
@@ -14,6 +14,6 @@ for find_count in find :
         print(dic[find_count],end=' ')
     else :
         print(0,end=' ')
-## 이문제는 binary Search를 통해서 풀어야 하지만
-## count 문제로 나옴으로써 꼬아짐
+## 이문제는 binary Search를 통해서 풀어야 하지만 리스트로 binary search를 해도 안풀림
 ## 오히려 binary Search를 안써도 풀리는 문제이며, 굳이 binary search를 쓴다면 dict 값을 찾는 용도로는 가능
+## 파이썬 기본 라이브러리 bisect을 통해 풀면 된다고함?? https://hongcoding.tistory.com/12
